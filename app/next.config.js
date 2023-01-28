d6c00c093d7e92a5d58a4808c71b198f4323ae1b
Leejjon <leejjon@gmail.com>
2023-01-28 14:07:41 +0100

    Forgot the path import.

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
   experimental: {
-    outputFileTracingRoot: ".."
+    outputFileTracingRoot: path.join(__dirname, "..")
   }
 }
 
